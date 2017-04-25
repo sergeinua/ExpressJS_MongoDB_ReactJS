8660f1e10039788a53df9b54b2cc4700a54c687b
sergei <sergeie51@gmail.com>
2017-04-25 16:15:33 +0300

    map markers for item create/update finished

--- public/dist/js/function.js
+++ public/dist/js/function.js
@@ -1,84 +1,95 @@
 function deletePage(pageId) {
     $.ajax({ url: '/admin/page/' + pageId, type: 'delete'})
         .success(function (result) {
-            console.log('+');
             window.location.reload();
         })
         .error(function (error) {
-            console.log('-');
             console.log('Error occurred while deleting page', error);
         });
 }
+/* Google maps */
+var map;
+var markers = [];
+var marker;
+//initial setup
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -33.8688, lng: 151.2195},
-        zoom: 13
+    var haightAshbury = {lat: 50.424, lng: 30.569};
+    //setting map
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: haightAshbury
+    });
+    // This event listener will call addMarker() when the map is clicked.
+    map.addListener('click', function(event) {
+        deleteMarkers();
+        addMarker(event.latLng);
+        map.setCenter(marker.getPosition());
     });
 
     var input = document.getElementById('pac-input');
-
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
-
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
     var infowindow = new google.maps.InfoWindow();
     var infowindowContent = document.getElementById('infowindow-content');
     infowindow.setContent(infowindowContent);
-    var marker = new google.maps.Marker({
-        map: map
-    });
-    marker.addListener('click', function () {
-        infowindow.open(map, marker);
-    });
-
     autocomplete.addListener('place_changed', function () {
         infowindow.close();
         var place = autocomplete.getPlace();
         if (!place.geometry) {
             return;
         }
-
         if (place.geometry.viewport) {
             map.fitBounds(place.geometry.viewport);
         } else {
             map.setCenter(place.geometry.location);
             map.setZoom(20);
         }
-
-        // Set the position of the marker using the place ID and location.
-//            marker.setPlace({
-//                placeId: place.place_id,
-//                location: place.geometry.location
-//            });
-//            marker.setVisible(true);
-
         infowindowContent.children['place-name'].textContent = place.name;
         infowindowContent.children['place-id'].textContent = place.place_id;
         infowindowContent.children['place-address'].textContent = place.formatted_address;
-        infowindow.open(map, marker);
+        // infowindow.open(map, marker);
     });
-    google.maps.event.addListener(map, 'click', function(event) {
-        //adding marker
-        placeMarker(event.latLng);
-        var coordinates = JSON.parse(JSON.stringify(event.latLng));
-        //updating coordinates
-        $('#lat').val(coordinates.lat);
-        $('#lng').val(coordinates.lng);
-        //getting address string by coordinates
-        var _url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+
-            coordinates.lat + ',' + coordinates.lng + '&sensor=true';
-        $.get(_url, function (resp) {
-            $('#address').val(resp.results["0"].formatted_address);
-        });
+}
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+    marker = new google.maps.Marker({
+        position: location,
+        map: map
     });
-    function placeMarker(location) {
-        var marker = new google.maps.Marker({
-            position: location,
-            map: map
-        });
+    markers.push(marker);
+    //updating coordinates
+    var coordinates = JSON.parse(JSON.stringify(location));
+    $('#lat').val(coordinates.lat);
+    $('#lng').val(coordinates.lng);
+    //getting address string by coordinates
+    var _url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+
+        coordinates.lat + ',' + coordinates.lng + '&sensor=true';
+    $.get(_url, function (resp) {
+        $('#address').val(resp.results["0"].formatted_address);
+    });
+}
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
     }
 }
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    setMapOnAll(null);
+}
+// Shows any markers currently in the array.
+function showMarkers() {
+    setMapOnAll(map);
+}
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+
 $(document).ready(function () {
     $('#sign-out').on('click', function (e) {
         e.preventDefault();
@@ -119,5 +130,14 @@ $(document).ready(function () {
             $('#pics').val(filename);
         }
         $('#empty-img').remove();
+    }
+    //placing existing marker
+    var _lat = parseFloat($('#lat').val()),
+        _lng = parseFloat($('#lng').val());
+    if (_lat && _lng) {
+        setTimeout(function () {
+            addMarker({lat: _lat, lng: _lng});
+            map.setCenter({lat: _lat, lng: _lng});
+        }, 1000);
     }
 });
