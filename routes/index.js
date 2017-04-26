6d15e485feaf996b4adad66a123756bfacf4297d
sergei <sergeie51@gmail.com>
2017-04-26 17:24:00 +0300

    front page: html some rubbish removed, template not finished

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,23 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-var User = require('../models/user');
+var Item = require('../models/item');
 
-/* GET home page. */
+//home page
 router.get('/', function(req, res, next) {
-    res.render('pages/index');
+    Item.find(function (err, items) {
+        res.render('pages/index', { items: items });
+    });
+});
+
+//getting all item objects for further placing markers
+router.post('/', function (req, res, next) {
+    Item.find(function (err, items) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.status(200).send(items);
+    });
 });
 
 module.exports = router;
