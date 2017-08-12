f1999272b7cf0cf07db5a242724a02dea3b853aa
Sergei <sergei.nikonov@yahoo.com>
2017-08-12 10:39:15 +0300

    bootstrap implemented, home page: district dropdown implemented

--- public/rapp/src/pages/Home-page.jsx
+++ public/rapp/src/pages/Home-page.jsx
@@ -1,17 +1,74 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { ButtonGroup, Button, DropdownButton, MenuItem } from 'react-bootstrap';
 
 import './Home-page.css';
+import Menu from '../components/Menu';
+import * as homeBackground from '../homeBackground.jpg';
 
 class HomePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            districts: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('/district', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }})
+            .then(resp => resp.json())
+            .then(resp => {
+                this.setState({districts: resp});
+            });
+    }
+
+    handleDistrictItemClick(districtName) {
+        let district = districtName.replace(' ', '+'),
+            url = `https://maps.googleapis.com/maps/api/geocode/json?address=${district}&key=AIzaSyB4MDQL1d2dTKJQBq-alxGtbE8mCu-CLDk&language=ru`;
+        console.log(url);
+        fetch(url)
+            .then(resp => resp.json())
+            .then((resp) => {
+                console.log('resp',resp)
+            });
     }
 
     render() {
+        let image = '/img/homeBackground.jpg';
+        let divStyle = {
+            backgroundImage: `url(${image})`,
+            backgroundSize: 'cover',
+            backgroundPosition: '50% 20%',
+            minHeight: 700 + 'px'
+        };
         return (
-            <div>
-                <Link to="/map">Map</Link>
+            <div className="AppTemplate">
+                <div className="FrontPage">
+                    <Menu/>
+                </div>
+                <div className="HomeHero">
+                    <div className="HomeHero-hero-wrapper">
+                        <div className="HomeHero-hero" style={divStyle}>
+                            {this.state.districts.length > 0 &&
+                                <div className="Row">
+                                    <ButtonGroup>
+                                        <DropdownButton title="Dropdown" id="bg-nested-dropdown">
+                                            {this.state.districts.map((district) => {
+                                                return (<MenuItem
+                                                            onClick={() => this.handleDistrictItemClick(district)}>{district}</MenuItem>);
+                                            })}
+                                        </DropdownButton>
+                                    </ButtonGroup>
+                                </div>
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
