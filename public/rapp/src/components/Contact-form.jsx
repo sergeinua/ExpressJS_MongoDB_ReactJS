9cfe4184959a35be2dd521f517adbcfeab56b21c
Sergei Nikonov <sergei.nikonov@yahoo.com>
2017-09-07 17:22:43 +0300

    Contact form: implemented without api request

--- public/rapp/src/components/Contact-form.jsx
+++ public/rapp/src/components/Contact-form.jsx
@@ -1,8 +1,59 @@
 import React, { Component } from 'react';
 
 class ContactForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: null,
+            telephone: null,
+            email: null,
+            message: null,
+            errorName: false,
+            errorTelephone: false,
+            errorMessage: false
+        };
+        this.handleFieldChange = this.handleFieldChange.bind(this);
+        this.handleSubmitForm = this.handleSubmitForm.bind(this);
+    }
+
+    handleSubmitForm() {
+        if (this.validateForm()) {
+            console.log('sending form');
+        }
+    }
+
+    handleFieldChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    validateForm() {
+        let errorState,
+            errorCnt = 0;
+        if (this.state.name && this.state.name.length > 0) {
+            errorState = false;
+        } else {
+            errorState = true;
+            errorCnt++;
+        }
+        this.setState({errorName: errorState});
+        if (this.state.telephone && this.state.telephone.length > 0) {
+            errorState = false;
+        } else {
+            errorState = true;
+            errorCnt++;
+        }
+        this.setState({errorTelephone: errorState});
+        if (this.state.message && this.state.message.length > 0) {
+            errorState = false;
+        } else {
+            errorState = true;
+            errorCnt++;
+        }
+        this.setState({errorMessage: errorState});
+        return errorCnt == 0;
+    }
+
     render() {
-        let formData = 'q';
         return (
             <div className="Container Container-md" style={{margin: 'auto'}}>
                 <div className="Row Row-md">
@@ -13,28 +64,44 @@ class ContactForm extends Component {
                                 <div className="Input">
                                     <label className="Label">Your Message</label>
                                     <textarea
-                                        maxlength="750" required="" type="textarea"
+                                        maxlength="750"
+                                        required=""
+                                        type="textarea"
+                                        name="message"
                                         className="Input-element Input-textarea contactFormInput"
-                                        style={{height: 58 + 'px', width: 100 + '%'}}>I'd like to schedule a viewing for Oak Park City Apartments. Please contact me with more information!</textarea>
+                                        onChange={this.handleFieldChange}
+                                        value={this.state.message}
+                                        style={{height: 58 + 'px', width: 100 + '%'}}>{this.state.message}</textarea>
+                                    {this.state.errorMessage &&
+                                        <p className="Text Utils-alert Text-sm">Message can't be empty</p>
+                                    }
                                 </div>
                             </div>
                             <div className="Row Row-sm">
                                 <div className="Input Contact-message-box">
                                     <label className="Label">Your email</label>
-                                    <input type="email"
+                                    <input
+                                        type="email"
                                         className="Input-element Utils-text-overflow contactFormInput"
                                         maxlength="50"
-                                        required=""
                                         name="email"
-                                        value=""/>
+                                        onChange={this.handleFieldChange}
+                                        value={this.state.email}/>
                                 </div>
                             </div>
                             <div className="Row">
                                 <div className="Input">
                                     <label className="Label">Your name</label>
                                     <input
-                                        maxlength="50" required="" value=""
-                                        className="Input-element Utils-text-overflow contactFormInput"/>
+                                        maxlength="50"
+                                        required="required"
+                                        name="name"
+                                        className="Input-element Utils-text-overflow contactFormInput"
+                                        onChange={this.handleFieldChange}
+                                        value={this.state.name}/>
+                                    {this.state.errorName &&
+                                        <p className="Text Utils-alert Text-sm">Name can't be empty</p>
+                                    }
                                 </div>
                             </div>
                             <div className="Row Row-sm">
@@ -42,17 +109,22 @@ class ContactForm extends Component {
                                     <label className="Label">Your phone number</label>
                                     <input type="tel"
                                        maxlength="15"
-                                       required=""
-                                       value=""
+                                       required="required"
+                                       name="telephone"
+                                       value={this.state.telephone}
+                                       onChange={this.handleFieldChange}
                                        placeholder="(123) 456-7890"
                                        className="Input-element Utils-text-overflow contactFormInput"/>
+                                    {this.state.errorTelephone &&
+                                        <p className="Text Utils-alert Text-sm">Telephone can't be empty</p>
+                                    }
                                 </div>
                             </div>
                             <div className="Contact-submit-btn">
                                 <button
                                     type="button"
                                     className="Button Button-lg Button-text-color Button-full"
-                                    onClick={() => this.props.handleSubmitForm(formData)}>
+                                    onClick={this.handleSubmitForm}>
                                     Send Message
                                 </button>
                             </div>
